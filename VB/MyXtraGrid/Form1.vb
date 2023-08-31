44461ba16932509c0c0454ff62bf9e911c5b7e32
DevExpressExampleBot <fakeEmail>
2023-08-31 13:09:32 +0400

    Source auto update [skip ci]

--- VB/MyXtraGrid/Form1.vb
+++ VB/MyXtraGrid/Form1.vb
@@ -1,28 +1,28 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 
 Namespace MyXtraGrid
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			Dim dt As New DataTable()
-			dt.Columns.Add("ID", GetType(Integer))
-			dt.Columns.Add("Name")
-			dt.Columns.Add("Date", GetType(DateTime))
-			For i As Integer = 0 To 14
-				dt.Rows.Add(New Object() { i, String.Format("Name {0}", i),DateTime.Today.AddDays(i) })
-			Next i
-			myGridControl1.DataSource = dt
-		End Sub
-	End Class
+    Public Partial Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            Dim dt As DataTable = New DataTable()
+            dt.Columns.Add("ID", GetType(Integer))
+            dt.Columns.Add("Name")
+            dt.Columns.Add("Date", GetType(Date))
+            For i As Integer = 0 To 15 - 1
+                dt.Rows.Add(New Object() {i, String.Format("Name {0}", i), Date.Today.AddDays(i)})
+            Next
+
+            myGridControl1.DataSource = dt
+        End Sub
+    End Class
 End Namespace
--- VB/MyXtraGrid/MyGridView.vb
+++ VB/MyXtraGrid/MyGridView.vb
@@ -1,4 +1,3 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports DevExpress.XtraGrid.Views.Base
 Imports DevExpress.XtraEditors.Repository
@@ -6,124 +5,120 @@ Imports DevExpress.XtraGrid.Columns
 Imports DevExpress.XtraEditors.Helpers
 Imports System.Windows.Forms
 Imports System.Drawing
-Imports DevExpress.Utils
-Imports DevExpress.Data
 Imports System.Collections.Generic
 
 Namespace MyXtraGrid
-	Public Class MyGridView
-		Inherits DevExpress.XtraGrid.Views.Grid.GridView
-		Public Sub New()
-			Me.New(Nothing)
-		End Sub
-		Public Sub New(ByVal grid As DevExpress.XtraGrid.GridControl)
-			MyBase.New(grid)
-		End Sub
-		Protected Overrides ReadOnly Property ViewName() As String
-			Get
-				Return "MyGridView"
-			End Get
-		End Property
 
-		Protected Overrides Function CreateDateFilterPopup(ByVal column As DevExpress.XtraGrid.Columns.GridColumn, ByVal ownerControl As System.Windows.Forms.Control, ByVal creator As Object) As DateFilterPopup
-			Return New MyDateFilterPopup(Me, column, ownerControl, creator)
-		End Function
-	End Class
+    Public Class MyGridView
+        Inherits DevExpress.XtraGrid.Views.Grid.GridView
 
-	Public Class MyDateFilterPopup
-		Inherits DateFilterPopup
+        Public Sub New()
+            Me.New(Nothing)
+        End Sub
 
-		Public Sub New(ByVal view As ColumnView, ByVal column As GridColumn, ByVal ownerControl As System.Windows.Forms.Control, ByVal creator As Object)
-			MyBase.New(view, column, ownerControl, creator)
-		End Sub
+        Public Sub New(ByVal grid As DevExpress.XtraGrid.GridControl)
+            MyBase.New(grid)
+        End Sub
 
-		Private dateCalendar_Renamed As DateControlEx = Nothing
-		Private ReadOnly Property DateCalendar() As DateControlEx
-			Get
-				If dateCalendar_Renamed Is Nothing Then
-					SetDateCalendar(DateFilterControl)
-				End If
-				Return dateCalendar_Renamed
-			End Get
-		End Property
-		Private dateFilterControl_Renamed As PopupOutlookDateFilterControl = Nothing
-		Private ReadOnly Property DateFilterControl() As PopupOutlookDateFilterControl
-			Get
-				If dateFilterControl_Renamed Is Nothing Then
-					SetDateFilterControl(item)
-				End If
-				Return dateFilterControl_Renamed
-			End Get
-		End Property
-		Private item As RepositoryItemPopupContainerEdit = Nothing
+        Protected Overrides ReadOnly Property ViewName As String
+            Get
+                Return "MyGridView"
+            End Get
+        End Property
 
-		Protected Overrides Function CreateRepositoryItem() As RepositoryItemPopupBase
-			item = TryCast(MyBase.CreateRepositoryItem(), RepositoryItemPopupContainerEdit)
-			AddHandler DateCalendar.CustomDrawDayNumberCell, AddressOf OnCustomDrawDayNumberCell
-			Return item
-		End Function
+        Protected Overrides Function CreateDateFilterPopup(ByVal column As GridColumn, ByVal ownerControl As Control, ByVal creator As Object) As DateFilterPopup
+            Return New MyDateFilterPopup(Me, column, ownerControl, creator)
+        End Function
+    End Class
 
-		Private Sub SetDateFilterControl(ByVal item As RepositoryItemPopupContainerEdit)
-			For Each ctrl As Control In item.PopupControl.Controls
-				If TypeOf ctrl Is PopupOutlookDateFilterControl Then
-					dateFilterControl_Renamed = TryCast(ctrl, PopupOutlookDateFilterControl)
-					Exit For
-				End If
-			Next ctrl
-		End Sub
+    Public Class MyDateFilterPopup
+        Inherits DateFilterPopup
 
-		Private Sub SetDateCalendar(ByVal dateFilterControl As PopupOutlookDateFilterControl)
-			For Each c As Control In dateFilterControl.Controls
-				If TypeOf c Is DateControlEx Then
-					dateCalendar_Renamed = TryCast(c, DateControlEx)
-					Exit For
-				End If
-			Next c
-		End Sub
+        Public Sub New(ByVal view As ColumnView, ByVal column As GridColumn, ByVal ownerControl As Control, ByVal creator As Object)
+            MyBase.New(view, column, ownerControl, creator)
+        End Sub
 
-		Protected Shadows ReadOnly Property View() As MyGridView
-			Get
-				Return TryCast(MyBase.View, MyGridView)
-			End Get
-		End Property
+        Private dateCalendarField As DateControlEx = Nothing
 
-		Private uniqueFilterPopupValues_Renamed() As Object = Nothing
-		Private ReadOnly Property UniqueFilterPopupValues() As Object()
-			Get
-				If uniqueFilterPopupValues_Renamed Is Nothing Then
-					uniqueFilterPopupValues_Renamed = GetUniqueFilterPopupValues()
-				End If
-				Return uniqueFilterPopupValues_Renamed
-			End Get
-		End Property
+        Private ReadOnly Property DateCalendar As DateControlEx
+            Get
+                If dateCalendarField Is Nothing Then SetDateCalendar(DateFilterControl)
+                Return dateCalendarField
+            End Get
+        End Property
 
-		Private Function GetUniqueFilterPopupValues() As Object()
-			Dim dates As New List(Of Object)()
-			For i As Integer = 0 To View.DataRowCount - 1
-				Dim val As Object = View.GetRowCellValue(i, Column)
-				If (Not dates.Contains(val)) Then
-					dates.Add(val)
-				End If
-			Next i
-			Return dates.ToArray()
-		End Function
+        Private dateFilterControlField As PopupOutlookDateFilterControl = Nothing
 
-		Private Sub OnCustomDrawDayNumberCell(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Calendar.CustomDrawDayNumberCellEventArgs)
-			If IsExistingDate(e.Date) Then
-				e.Style.ForeColor = Color.BlueViolet
-			End If
-		End Sub
+        Private ReadOnly Property DateFilterControl As PopupOutlookDateFilterControl
+            Get
+                If dateFilterControlField Is Nothing Then SetDateFilterControl(item)
+                Return dateFilterControlField
+            End Get
+        End Property
 
-		Private Function IsExistingDate(ByVal dt As DateTime) As Boolean
-			Dim index As Integer = Array.IndexOf(UniqueFilterPopupValues, dt)
-			Return index <> -1
-		End Function
+        Private item As RepositoryItemPopupContainerEdit = Nothing
 
-		Public Overrides Sub Dispose()
-			If DateCalendar IsNot Nothing Then
-				RemoveHandler DateCalendar.CustomDrawDayNumberCell, AddressOf OnCustomDrawDayNumberCell
-			End If
-			MyBase.Dispose()
-		End Sub
-	End Class
+        Protected Overrides Function CreateRepositoryItem() As RepositoryItemPopupBase
+            item = TryCast(MyBase.CreateRepositoryItem(), RepositoryItemPopupContainerEdit)
+            AddHandler DateCalendar.CustomDrawDayNumberCell, AddressOf OnCustomDrawDayNumberCell
+            Return item
+        End Function
+
+        Private Sub SetDateFilterControl(ByVal item As RepositoryItemPopupContainerEdit)
+            For Each ctrl As Control In item.PopupControl.Controls
+                If TypeOf ctrl Is PopupOutlookDateFilterControl Then
+                    dateFilterControlField = TryCast(ctrl, PopupOutlookDateFilterControl)
+                    Exit For
+                End If
+            Next
+        End Sub
+
+        Private Sub SetDateCalendar(ByVal dateFilterControl As PopupOutlookDateFilterControl)
+            For Each c As Control In dateFilterControl.Controls
+                If TypeOf c Is DateControlEx Then
+                    dateCalendarField = TryCast(c, DateControlEx)
+                    Exit For
+                End If
+            Next
+        End Sub
+
+        Protected Overloads ReadOnly Property View As MyGridView
+            Get
+                Return TryCast(MyBase.View, MyGridView)
+            End Get
+        End Property
+
+        Private uniqueFilterPopupValuesField As Object() = Nothing
+
+        Private ReadOnly Property UniqueFilterPopupValues As Object()
+            Get
+                If uniqueFilterPopupValuesField Is Nothing Then uniqueFilterPopupValuesField = GetUniqueFilterPopupValues()
+                Return uniqueFilterPopupValuesField
+            End Get
+        End Property
+
+        Private Function GetUniqueFilterPopupValues() As Object()
+            Dim dates As List(Of Object) = New List(Of Object)()
+            For i As Integer = 0 To View.DataRowCount - 1
+                Dim val As Object = View.GetRowCellValue(i, Column)
+                If Not dates.Contains(val) Then dates.Add(val)
+            Next
+
+            Return dates.ToArray()
+        End Function
+
+        Private Sub OnCustomDrawDayNumberCell(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Calendar.CustomDrawDayNumberCellEventArgs)
+            If IsExistingDate(e.Date) Then e.Style.ForeColor = Color.BlueViolet
+        End Sub
+
+        Private Function IsExistingDate(ByVal dt As Date) As Boolean
+            Dim index As Integer = Array.IndexOf(UniqueFilterPopupValues, dt)
+            Return index <> -1
+        End Function
+
+        Public Overrides Sub Dispose()
+            If DateCalendar IsNot Nothing Then RemoveHandler DateCalendar.CustomDrawDayNumberCell, AddressOf OnCustomDrawDayNumberCell
+            MyBase.Dispose()
+        End Sub
+    End Class
 End Namespace
